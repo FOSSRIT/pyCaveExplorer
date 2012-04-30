bef51ee0017fb36e41e6bfacc0f1799e474b9b71
David Gay <oddshocks@gmail.com>
2012-04-30 12:58:57 -0400

    Fixed TypeError

--- pyCaveExplorer/element.py
+++ pyCaveExplorer/element.py
@@ -3,7 +3,7 @@ A game element in pyCaveExplorer
 Anything that is placed on the grid MUST extend this class!
 '''
 
-class GameElement:
+class GameElement(object):
     def __init__(self, gridX=0, gridY=0):
         self.gridX = gridX # x position on grid
         self.gridY = gridY # y position on grid
