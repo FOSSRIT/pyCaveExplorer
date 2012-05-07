2f850a13e472f37981997db05eb43c4c3bed51f1
David Gay <oddshocks@gmail.com>
2012-05-07 11:46:35 -0400

    Game start code in progress

--- pyCaveExplorer/game.py
+++ pyCaveExplorer/game.py
@@ -4,9 +4,16 @@ Also manages all GameElement objects
 '''
 
 import pygame
+from constants import *
 
 class Game:
-    def __init__(self): 
+    def __init__(self):
+        self.window = pygame.Surface((GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT))
+        self.window.fill(COLOR_BROWN)
         # Set up grid
         # Solver stuff
+        self.draw()
+    def draw(self):
+        # Draw the grid
+        pygame.draw.line(self.window, COLOR_GREY, (100, 0), (100, 300), 4) 
         pass
--- pyCaveExplorer/main.py
+++ pyCaveExplorer/main.py
@@ -40,7 +40,7 @@ def display_menu():
     screen.blit(title_text, (200, 200))
 
 def start_game():
-    screen.fill(COLOR_BROWN)
+    game.draw()
 
 def main():
     """ This is the main entry point for the game.
