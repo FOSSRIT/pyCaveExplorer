61c588506f4c80245d34b75faee305fe728b76ee
Jennifer Dziuba <insanefastone@gmail.com>
2012-05-21 23:11:22 -0400

    missed one file

--- pyCaveExplorer/shadows.py
+++ pyCaveExplorer/shadows.py
@@ -2,7 +2,10 @@
 Controls shadows on grid tiles
 '''
 
+from constants import *
+from element import GameElement
+
 class Shadows(GameElement):
-	def __init__(self):
-		self.darkness = 1 # how dark the square is, between 0 and 1
-		pass
+    def __init__(self, *args, **kwargs):
+        super(Shadows, self).__init__(*args, **kwargs)
+        self.darkness = LIGHT_LOW # how dark the tile is, between 0 and 255
